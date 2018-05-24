31d3641c887dd5a986d5c024bc5dc73b749882ea
xiaosean <xiaosean@users.noreply.github.com>
2018-05-24 23:38:10 +0800

    discrimnator change to SELU

--- model/discriminator.py
+++ model/discriminator.py
@@ -12,23 +12,39 @@ class FCDiscriminator(nn.Module):
 		self.conv3 = nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1)
 		self.conv4 = nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=2, padding=1)
 		self.classifier = nn.Conv2d(ndf*8, 1, kernel_size=4, stride=2, padding=1)
-
 		self.leaky_relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
-		#self.up_sample = nn.Upsample(scale_factor=32, mode='bilinear')
+		self.selu = nn.SELU(inplace=True)
+
+#self.up_sample = nn.Upsample(scale_factor=32, mode='bilinear')
 		#self.sigmoid = nn.Sigmoid()
 
-
+	#	discriminator revised selu 20180524
 	def forward(self, x):
 		x = self.conv1(x)
-		x = self.leaky_relu(x)
+		x = self.selu(x)
 		x = self.conv2(x)
-		x = self.leaky_relu(x)
+		x = self.selu(x)
 		x = self.conv3(x)
-		x = self.leaky_relu(x)
+		x = self.selu(x)
 		x = self.conv4(x)
-		x = self.leaky_relu(x)
+		x = self.selu(x)
 		x = self.classifier(x)
 		#x = self.up_sample(x)
-		#x = self.sigmoid(x) 
+		#x = self.sigmoid(x)
 
 		return x
+#	origin 20180524
+	# def forward(self, x):
+	# 	x = self.conv1(x)
+	# 	x = self.leaky_relu(x)
+	# 	x = self.conv2(x)
+	# 	x = self.leaky_relu(x)
+	# 	x = self.conv3(x)
+	# 	x = self.leaky_relu(x)
+	# 	x = self.conv4(x)
+	# 	x = self.leaky_relu(x)
+	# 	x = self.classifier(x)
+	# 	#x = self.up_sample(x)
+	# 	#x = self.sigmoid(x)
+    #
+	# 	return x
