28225e1137f1f8511f0e5e37fedb643d8d689259
xiaosean <xiaosean@users.noreply.github.com>
2018-12-23 02:58:13 +0800

    Add:CoordConv

--- model/sp_cgan_discriminator.py
+++ model/sp_cgan_discriminator.py
@@ -18,7 +18,10 @@ class SP_CGAN_FCDiscriminator(nn.Module):
 		self.gamma = nn.Parameter(torch.zeros(num_classes))
 
 
-		self.conv1 = spectral_norm(nn.Conv2d(num_classes, ndf, kernel_size=4, stride=2, padding=1))
+		# self.conv1 = spectral_norm(nn.Conv2d(num_classes, ndf, kernel_size=4, stride=2, padding=1))
+		self.conv1 = spectral_norm(nn.Conv2d(num_classes+2, ndf, kernel_size=4, stride=2, padding=1))
+
+
 		self.conv2 = spectral_norm(nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1))
 		self.conv3 = spectral_norm(nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1))
 		self.conv4 = spectral_norm(nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=2, padding=1))
@@ -60,7 +63,25 @@ class SP_CGAN_FCDiscriminator(nn.Module):
 		# tensor_sprior = tensor_sprior.double()
 		return tensor_sprior
 
+	def _add_xy_index_channels(self, img):
+		h_s = img.shape[-2]
+		w_s = img.shape[-1]
+		c_y_index = np.expand_dims(np.tile(np.arange(h_s), w_s).reshape(w_s, h_s).T, 2) / (h_s - 1)
+		c_x_index = np.tile(np.arange(w_s), h_s).T.reshape(h_s, w_s, 1) / (w_s - 1)
+		c_y_index = torch.tensor(c_y_index,
+									 dtype=torch.float64,
+									 device=torch.device('cuda:0')).float().cuda().view(1, 1, h_s, w_s)
+		c_x_index = torch.tensor(c_x_index,
+									 dtype=torch.float64,
+									 device=torch.device('cuda:0')).float().cuda().view(1, 1, h_s, w_s)
+
+		# img_extended = np.concatenate([img, c_y_index, c_x_index], 2).copy()
+		img_extended = torch.cat([img, c_y_index, c_x_index], 1)
+
+		return img_extended
+
 	def forward(self, x, label=None):
+		x = self._add_xy_index_channels(x)
 		x = self.conv1(x)
 		x = self.activation(x)
 		x = self.conv2(x)
